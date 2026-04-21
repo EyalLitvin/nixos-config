725e03411249c203233f31fcb552bde11b2b9bbb
EyalLitvin <eyalitvin@email.com>
2026-04-21 21:34:55 +0300

    added wallpapers! (still not actively the wallpaper of the system)

--- home/yazi.nix
+++ home/yazi.nix
@@ -7,5 +7,14 @@
     settings = {
       mgr.show_hidden = true;
     };
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = [ "<C-w>" ];
+          run = ''shell -- bash -c 'cp "$1" ~/.dotfiles/wallpapers/' _ "$1"'';
+          desc = "Copy to wallpapers";
+        }
+      ];
+    };
   };
 }
